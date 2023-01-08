d6b26da2b400cd4dd0f1e05fbb62d869eea893db
Imani Tyson <imanistyson@gmail.com>
2023-01-08 00:02:01 +0000

    Saved progress on Average 2023-01-08 00:02:01 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,6 +18,12 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum = sum + nums[i];
+        }
+        double sum2 = (double) sum;
+        double avg = sum2/nums.length;
+        return avg;
     }
 }
