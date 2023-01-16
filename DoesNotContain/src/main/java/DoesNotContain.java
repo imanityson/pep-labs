e396759fed8867cd18a87a9054edadb5c7a5ad56
Imani Tyson <imanistyson@gmail.com>
2023-01-16 00:30:29 +0000

    Saved progress on DoesNotContain 2023-01-16 00:30:29 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -13,6 +13,18 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        return false;
+        boolean match = false;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == target){
+                match = true;
+            } else {
+                match = match;
+            }
+        }
+        if(match == true){
+            return false;
+        } else {
+            return true; 
+        }
     }
 }
