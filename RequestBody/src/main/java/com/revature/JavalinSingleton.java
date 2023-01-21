59382b43070d8110f5a599273ab480379f579385
Imani Tyson <imanistyson@gmail.com>
2023-01-21 00:39:46 +0000

    Saved progress on Truncate 2023-01-21 00:39:46 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -17,6 +17,8 @@ public class JavalinSingleton {
          * Note: Please refer to the "RequestBody.MD" file for more assistance if needed.
          */
         app.post("/problem1", ctx -> {
+            String jsonString = ctx.body();
+            
                 //implement logic here
         });
 
