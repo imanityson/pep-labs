7ffd26bf07a973ead08a666fc8369faa440f784c
Imani Tyson <imanistyson@gmail.com>
2023-01-21 01:49:27 +0000

    Saved progress on IsSorted 2023-01-21 01:49:27 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -7,6 +7,20 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        int prevNum = 0;
+        int sortCounter = 0;
+        for(int i = 0; i < arr.length; i++){
+           prevNum = i - prevNum;
+            if(arr[i] >= arr[prevNum]){
+                sortCounter = sortCounter + 1;
+            }
+           prevNum++;
+        }
+        if(sortCounter == arr.length){
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
